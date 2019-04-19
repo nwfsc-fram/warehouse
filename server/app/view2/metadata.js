f27e86d6cb61bba294a457c6ac12a7b05da075f0
Seth Gerou <seth.gerou@noaa.gov>
2019-04-19 11:19:02 -0700

    testing metadata links fix

--- server/app/view2/metadata.js
+++ server/app/view2/metadata.js
@@ -55,8 +55,7 @@ angular.module('myApp.metadata', ['ngRoute'])
                 $scope.yearsOfData = data[i].years;
                 $scope.pointOfContact = data[i].contact.replace("<","").replace(">","");
 
-                console.log(data[i].links)
-                if (data[i].links.length==2)
+                if (data[i].links.length>2)
                 {
                     $scope.json_url = data[i].links[0].href;
                     $scope.csv_url = data[i].links[1].href;
